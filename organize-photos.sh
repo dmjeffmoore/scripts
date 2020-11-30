3fc19a4346c8d2c0becbd63d57812cc61f03a357
Jeffrey Moore <dmjeffmoore@gmail.com>
2020-11-29 17:14:26 -0700

    Update organize-photos.sh

--- organize-photos.sh
+++ organize-photos.sh
@@ -5,15 +5,16 @@
 # into the folder where all your unsorted photos are located and run it. While it is running, it will move pictures into ../Sorted
 # using the earliest date in the file's EXIF metadata.
 
+IFS=''
 NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in the current directory minus the script itself
 
 for file in *.{jpg,JPG,jpeg,png,gif}; do
-    [ -f "$file" ] || break # check if the file is actually a file
+    [ -f "$file" ] || break
     MIN_YEAR_MONTH=400000
 
     if exiftool -a -G1 -s -time:all $file > /dev/null # check if the exiftool command is successful on the file
     then
-        while IFS= read -r line
+        while read -r line
         do
             YEAR_MONTH=$(echo $line | cut -d ':' -f2,3 | xargs | sed 's/://')
             NUM_SEMICOLONS_IN_LINE=$(awk -F":" '{print NF-1}' <<< "${line}")
@@ -35,10 +36,12 @@ for file in *.{jpg,JPG,jpeg,png,gif}; do
             YEAR_MONTH=$(echo $MIN_YEAR_MONTH | sed 's/^\(.\{4\}\)/\1:/') # add the ':' to seperate the year and the month
             YEAR=$(echo $YEAR_MONTH | cut -d ':' -f 1)
             MONTH=$(echo $YEAR_MONTH | cut -d ':' -f 2)
+            if [ $MONTH == 0* ]; then
+                MONTH="${MONTH:1}"
+            fi
 
-            [ ! -d "../Sorted/$YEAR/$MONTH" ] && mkdir -p "../Sorted/$YEAR/$MONTH" # check if the directory exists - if not, create it
+            [ ! -d "../Sorted/$YEAR/$MONTH" ] && mkdir -p "../Sorted/$YEAR/$MONTH"
             mv $file ../Sorted/$YEAR/$MONTH
-            # echo "Moved $file to ../Sorted/$YEAR/$MONTH"
         else
             >&2 echo "Failed to get earliest date from EXIF data for $file"
         fi
@@ -48,7 +51,7 @@ for file in *.{jpg,JPG,jpeg,png,gif}; do
 
     NUM_FILES_IN_SORTED_DIR=$(find ../Sorted -type f | wc -l)
     percent=$(echo "scale=2;$NUM_FILES_IN_SORTED_DIR / $NUM_FILES_IN_CURR_DIR" | bc -l)
-    percent="${percent:1}" # remove the first character
+    percent="${percent:1}"
     echo -ne "progress: $percent%\r"
 done
 
