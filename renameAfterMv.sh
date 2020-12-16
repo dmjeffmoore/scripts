7070b0dbab1f1533791129fb5786029a9335b63d
Jeffrey Moore <dmjeffmoore@gmail.com>
2020-12-15 18:33:59 -0700

    Added a script to rename files after the mv backup linux command

---
+++ renameAfterMv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Description: This script will go through all files within the current directory and rename files
+# that result from the  mv --backup=numbered <source_file> <dest_file> to a name that keeps the original file extension.
+
+for file in *; do
+    [ -f "$file" ] || break
+
+    EXT=$(echo $file | cut -d '.' -f 3)
+    if [[ $EXT == "~"* ]]; then
+        FILE_NAME=$(echo $file | cut -d '.' -f 1)
+        EXTENSION=$(echo $file | cut -d '.' -f 2)
+        UNDERSCORE="_"
+        DOT="."
+        NUM=$(echo $file | cut -d '.' -f 3 | cut -d '~' -f 2)
+        NEW_NAME="$FILE_NAME$UNDERSCORE$NUM$DOT$EXTENSION"
+        mv $file $NEW_NAME
+    fi
+done
