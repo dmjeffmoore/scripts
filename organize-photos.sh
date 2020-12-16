470798e2a64b4f288f5c3c41e6cebe8a09d3c489
Jeffrey Moore <dmjeffmoore@gmail.com>
2020-12-15 17:52:18 -0700

    Refactored how organize-photos.sh echos progress, other small changes

--- organize-photos.sh
+++ organize-photos.sh
@@ -6,7 +6,10 @@
 # using the earliest date in the file's EXIF metadata.
 
 IFS=''
+DIGITS_REGEX='^[[:digit:]]+$'
+
 NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in the current directory minus the script itself
+echo "Sorting $NUM_FILES_IN_CURR_DIR files..."
 
 for file in *.{jpg,JPG,jpeg,png,gif}; do
     [ -f "$file" ] || break
@@ -19,12 +22,7 @@ for file in *.{jpg,JPG,jpeg,png,gif}; do
             YEAR_MONTH=$(echo $line | cut -d ':' -f2,3 | xargs | sed 's/://')
             NUM_SEMICOLONS_IN_LINE=$(awk -F":" '{print NF-1}' <<< "${line}")
 
-            re='^[[:digit:]]+$' # regex for digits
-
-            # check if the YEAR_MONTH string has more than 5 characters, that it only contains digits,
-            # and that the line does not contain ProfileDateTime which is a timestamp that we want to omit
-            # as it's not related to the file
-            if [ ${#YEAR_MONTH} -gt 5 ] && [[ $YEAR_MONTH =~ $re ]] && [ $NUM_SEMICOLONS_IN_LINE -gt 4 ] && [[ $line != *"ProfileDateTime"* ]]; then
+            if [ ${#YEAR_MONTH} -gt 5 ] && [[ $YEAR_MONTH =~ $DIGITS_REGEX ]] && [ $NUM_SEMICOLONS_IN_LINE -gt 4 ] && [[ $line != *"ProfileDateTime"* ]] && [[ $line != *"GPS"* ]]; then
                 if ([ $MIN_YEAR_MONTH -eq 400000 ] || [ $YEAR_MONTH -lt $MIN_YEAR_MONTH ]) && [ $YEAR_MONTH -ne 000000 ]; then
                     MIN_YEAR_MONTH=$YEAR_MONTH
                 fi
@@ -36,24 +34,18 @@ for file in *.{jpg,JPG,jpeg,png,gif}; do
             YEAR_MONTH=$(echo $MIN_YEAR_MONTH | sed 's/^\(.\{4\}\)/\1:/') # add the ':' to seperate the year and the month
             YEAR=$(echo $YEAR_MONTH | cut -d ':' -f 1)
             MONTH=$(echo $YEAR_MONTH | cut -d ':' -f 2)
-            if [ $MONTH == 0* ]; then
-                MONTH="${MONTH:1}"
-            fi
 
             [ ! -d "../Sorted/$YEAR/$MONTH" ] && mkdir -p "../Sorted/$YEAR/$MONTH"
             mv $file ../Sorted/$YEAR/$MONTH
+            NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in the current directory minus the script itself
+            echo "Moved $file, $NUM_FILES_IN_CURR_DIR files left"
         else
             >&2 echo "Failed to get earliest date from EXIF data for $file"
         fi
     else
         >&2 echo "Failed to extract EXIF data from $file"
     fi
-
-    NUM_FILES_IN_SORTED_DIR=$(find ../Sorted -type f | wc -l)
-    percent=$(echo "scale=2;$NUM_FILES_IN_SORTED_DIR / $NUM_FILES_IN_CURR_DIR" | bc -l)
-    percent="${percent:1}"
-    echo -ne "progress: $percent%\r"
 done
 
-echo -ne "\n"
+echo "Done!"
 exit 0
