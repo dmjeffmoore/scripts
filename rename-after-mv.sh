c8857dcdd27cf1054b8250bfb64e9fbc145e95ad
Jeffrey Moore <dmjeffmoore@gmail.com>
2020-12-18 16:04:03 -0700

    Fixed a bug where the rename script would stop if it encountered a directory

--- rename-after-mv.sh
+++ rename-after-mv.sh
@@ -4,16 +4,19 @@
 # that result from the  mv --backup=numbered <source_file> <dest_file> to a name that keeps the original file extension.
 
 for file in *; do
-    [ -f "$file" ] || break
-
-    EXT=$(echo $file | cut -d '.' -f 3)
-    if [[ $EXT == "~"* ]]; then
-        FILE_NAME=$(echo $file | cut -d '.' -f 1)
-        EXTENSION=$(echo $file | cut -d '.' -f 2)
-        UNDERSCORE="_"
-        DOT="."
-        NUM=$(echo $file | cut -d '.' -f 3 | cut -d '~' -f 2)
-        NEW_NAME="$FILE_NAME$UNDERSCORE$NUM$DOT$EXTENSION"
-        mv $file $NEW_NAME
+    if [ ! -d "$file" ]; then
+        EXT=$(echo "$file" | cut -d '.' -f 3)
+        if [[ $EXT == "~"* ]]; then
+            FILE_NAME=$(echo "$file" | cut -d '.' -f 1)
+            EXTENSION=$(echo "$file" | cut -d '.' -f 2)
+            UNDERSCORE="_"
+            DOT="."
+            NUM=$(echo "$file" | cut -d '.' -f 3 | cut -d '~' -f 2)
+            NEW_NAME="$FILE_NAME$UNDERSCORE$NUM$DOT$EXTENSION"
+            mv "$file" $NEW_NAME
+            echo "$file renamed to $NEW_NAME"
+        fi
     fi
 done
+
+echo "Done!"
