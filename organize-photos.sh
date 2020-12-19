0cc9f6ab94563113cd5072c08299e40f60530669
Jeffrey Moore <dmjeffmoore@gmail.com>
2020-12-19 11:26:01 -0700

    Fixed a bug where organize photos script would end if it encounterd a directory

--- organize-photos.sh
+++ organize-photos.sh
@@ -12,38 +12,39 @@ NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in 
 echo "Sorting $NUM_FILES_IN_CURR_DIR files..."
 
 for file in *.{jpg,JPG,jpeg,png,gif}; do
-    [ -f "$file" ] || break
-    MIN_YEAR_MONTH=400000
+    if [ ! -d "$file" ]; then
+        MIN_YEAR_MONTH=400000
 
-    if exiftool -a -G1 -s -time:all "$file" > /dev/null # check if the exiftool command is successful on the file
-    then
-        while read -r line
-        do
-            YEAR_MONTH=$(echo $line | cut -d ':' -f2,3 | xargs | sed 's/://')
-            NUM_SEMICOLONS_IN_LINE=$(awk -F":" '{print NF-1}' <<< "${line}")
-
-            if [ ${#YEAR_MONTH} -gt 5 ] && [[ $YEAR_MONTH =~ $DIGITS_REGEX ]] && [ $NUM_SEMICOLONS_IN_LINE -gt 4 ] && [[ $line != *"ProfileDateTime"* ]] && [[ $line != *"GPS"* ]]; then
-                if ([ $MIN_YEAR_MONTH -eq 400000 ] || [ $YEAR_MONTH -lt $MIN_YEAR_MONTH ]) && [ $YEAR_MONTH -ne 000000 ]; then
-                    MIN_YEAR_MONTH=$YEAR_MONTH
-                fi
-            fi
-        done < <(exiftool -a -G1 -s -time:all "$file")
-
-        if [ $MIN_YEAR_MONTH -ne 400000 ]
+        if exiftool -a -G1 -s -time:all "$file" > /dev/null # check if the exiftool command is successful on the file
         then
-            YEAR_MONTH=$(echo $MIN_YEAR_MONTH | sed 's/^\(.\{4\}\)/\1:/') # add the ':' to seperate the year and the month
-            YEAR=$(echo $YEAR_MONTH | cut -d ':' -f 1)
-            MONTH=$(echo $YEAR_MONTH | cut -d ':' -f 2)
+            while read -r line
+            do
+                YEAR_MONTH=$(echo $line | cut -d ':' -f2,3 | xargs | sed 's/://')
+                NUM_SEMICOLONS_IN_LINE=$(awk -F":" '{print NF-1}' <<< "${line}")
 
-            [ ! -d "../Sorted/$YEAR/$MONTH" ] && mkdir -p "../Sorted/$YEAR/$MONTH"
-            mv "$file" ../Sorted/$YEAR/$MONTH
-            NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in the current directory minus the script itself
-            echo "Moved $file, $NUM_FILES_IN_CURR_DIR files left"
+                if [ ${#YEAR_MONTH} -gt 5 ] && [[ $YEAR_MONTH =~ $DIGITS_REGEX ]] && [ $NUM_SEMICOLONS_IN_LINE -gt 4 ] && [[ $line != *"ProfileDateTime"* ]] && [[ $line != *"GPS"* ]]; then
+                    if ([ $MIN_YEAR_MONTH -eq 400000 ] || [ $YEAR_MONTH -lt $MIN_YEAR_MONTH ]) && [ $YEAR_MONTH -ne 000000 ]; then
+                        MIN_YEAR_MONTH=$YEAR_MONTH
+                    fi
+                fi
+            done < <(exiftool -a -G1 -s -time:all "$file")
+
+            if [ $MIN_YEAR_MONTH -ne 400000 ]
+            then
+                YEAR_MONTH=$(echo $MIN_YEAR_MONTH | sed 's/^\(.\{4\}\)/\1:/') # add the ':' to seperate the year and the month
+                YEAR=$(echo $YEAR_MONTH | cut -d ':' -f 1)
+                MONTH=$(echo $YEAR_MONTH | cut -d ':' -f 2)
+
+                [ ! -d "../Sorted/$YEAR/$MONTH" ] && mkdir -p "../Sorted/$YEAR/$MONTH"
+                mv "$file" ../Sorted/$YEAR/$MONTH
+                NUM_FILES_IN_CURR_DIR=$(expr $(ls | wc -l) - 1) # count the number of photos in the current directory minus the script itself
+                echo "Moved $file, $NUM_FILES_IN_CURR_DIR files left"
+            else
+                >&2 echo "Failed to get earliest date from EXIF data for $file"
+            fi
         else
-            >&2 echo "Failed to get earliest date from EXIF data for $file"
+            >&2 echo "Failed to extract EXIF data from $file"
         fi
-    else
-        >&2 echo "Failed to extract EXIF data from $file"
     fi
 done
 
